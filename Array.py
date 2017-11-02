b717ebd681d5cc33a076a09861984ceaf8256ae3
周楠 <906037502@qq.com>
2017-11-02 22:10:52 +0800

    增加Non-decreasing Array的解

--- Array.py
+++ Array.py
@@ -194,10 +194,26 @@ class Solution(object):
         nums[moves:] = nums[:-moves]
         nums[:moves] = tmp
 
+    def checkPossibility(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        times = 0
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                times += 1
+                if times == 2:
+                    return False
+                if i != 1 and i != len(nums) - 1:
+                    if nums[i - 1] > nums[i + 1] and nums[i] < nums[i - 2]:
+                        return False
+        return True
+
 
 if __name__ == '__main__':
     s = Solution()
     #print(s.getRowV2(3))
     #print(s.removeDuplicatesV2([1, 1, 2, 2, 3]))
-    print(s.rotateV2([1,2,3,4,5,6,7], 3))
+    print(s.checkPossibility([3, 4, 2, 3]))
 

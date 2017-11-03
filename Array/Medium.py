56d1337adecb7312704324c09429f0538e9f59a3
周楠 <906037502@qq.com>
2017-11-03 14:44:43 +0800

    Product of Array Except Self
    思路：
    
    构造两个数组相乘：
    
    [1, a1, a1*a2, a1*a2*a3]
    
    [a2*a3*a4, a3*a4, a4, 1]

--- Array/Medium.py
+++ Array/Medium.py
@@ -11,9 +11,25 @@ class Solution(object):
             ans.append(m)
         return ans + range(k + 2, n + 1)
 
+    def productExceptSelf(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        left = [1]
+        for i in range(len(nums) - 1):
+            left.append(nums[i] * left[-1])
+        right = [1]
+        nums.reverse()
+        for j in range(len(nums) - 1):
+            right.insert(0, nums[j] * right[0])
+        for index in range(len(nums)):
+            nums[index] = left[index] * right[index]
+        return nums
+
 
 if __name__ == '__main__':
     s = Solution()
     #print(s.getRowV2(3))
     #print(s.removeDuplicatesV2([1, 1, 2, 2, 3]))
-    print(s.constructArray(5, 4))
+    print(s.productExceptSelf([1,2,3,4,5]))

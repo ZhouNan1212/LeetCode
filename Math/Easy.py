960fe70f4e1d1045202ab70ee54916240e998e5b
周楠 <906037502@qq.com>
2017-11-21 15:57:42 +0800

    Self Dividing Numbers

--- Math/Easy.py
+++ Math/Easy.py
@@ -15,11 +15,26 @@ class Solution(object):
         """
         return 0 if num == 0 else (num - 1) % 9 + 1
 
+    def selfDividingNumbers_728(self, left, right):
+        """
+        :type left: int
+        :type right: int
+        :rtype: List[int]
+        """
+        re_list = []
+        for i in range(left, right + 1):
+            digit = list(str(i))
+            if "0" not in digit:
+                re = sum(map(lambda x: i % int(x), list(str(i))))
+                if re == 0:
+                    re_list.append(i)
+        return re_list
+
 if __name__ == '__main__':
     s = Solution()
     #print(s.getRowV2(3))
     #print(s.removeDuplicatesV2([1, 1, 2, 2, 3]))
-    print(s.addDigits_258_V2(99))
+    print(s.selfDividingNumbers_728(1,22))
 
 
 

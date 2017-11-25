d044ad6ab4238f2831f6e40ebbeea4f2c7a6a8b8
周楠 <906037502@qq.com>
2017-11-25 10:33:37 +0800

    Integer to Roman

--- Math/Medium.py
+++ Math/Medium.py
@@ -2,7 +2,7 @@
 
 
 class Solution(object):
-    def complexNumberMultiply_537(self, a, b):
+    def complexNumberMultiply_537_V1(self, a, b):
         """
         :type a: str
         :type b: str
@@ -17,11 +17,49 @@ class Solution(object):
         imaginary_part = a_real_part * b_imaginary_part + b_real_part * a_imaginary_part
         return str(real_part) + '+' + str(imaginary_part) + 'i'
 
+    def complexNumberMultiply_537_V2(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
+        a1, b1 = map(int, a[:-1].split("+"))
+        a2, b2 = map(int, b[:-1].split("+"))
+        return "%s+%si" % (a1*a2-b1*b2, a1*b2+a2*b1)
+
+    def intToRoman_12_V1(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        base = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1]
+        string = ["M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX",
+                  "V", "IV", "I"]
+        roman, i = "", 0
+        while num != 0:
+            if num >= base[i]:
+                num -= base[i]
+                roman += string[i]
+            else:
+                i += 1
+        return roman
+
+    def intToRoman_12_V2(self, num):
+        """
+        :type num: int
+        :rtype: str
+        """
+        M = ["", "M", "MM", "MMM"]
+        C = ["", "C", "CC", "CCC", "CD", "D", "DC", "DCC", "DCCC", "CM"]
+        X = ["", "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC"]
+        I = ["", "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX"]
+        return M[num / 1000] + C[(num % 1000) / 100] + X[(num % 100) / 10] + I[num % 10]
+
 if __name__ == '__main__':
     s = Solution()
     #print(s.getRowV2(3))
     #print(s.removeDuplicatesV2([1, 1, 2, 2, 3]))
-    print(s.complexNumberMultiply_537( "1+-1i", "1+-1i"))
+    print(s.intToRoman_12_V2(130))
 
 
 

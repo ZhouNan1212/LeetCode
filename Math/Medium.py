aa53612d26dd3ed5be9416ffb573e6b24d1e5aef
周楠 <906037502@qq.com>
2017-11-26 12:07:39 +0800

    Fraction Addition and Subtraction

--- Math/Medium.py
+++ Math/Medium.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import collections
-
+import sys
 
 class Solution(object):
     def complexNumberMultiply_537_V1(self, a, b):
@@ -120,13 +120,92 @@ class Solution(object):
                 return False
         return True
 
+    def nthUglyNumber_264_V1(self, n):  # 可用作归并
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        MAX_INT = sys.maxint
+        ugly_number = [
+            [1, 2] + [2 * i for i in range(1, n) if i % 2 == 0 or i % 3 == 0 or i % 5 == 0],  # two_ugly_number
+            [3] + [3 * i for i in range(1, n) if i % 2 != 0 and (i % 3 == 0 or i % 5 == 0)],  # three_ugly_number
+            [5] + [5 * i for i in range(1, n) if i % 2 != 0 and i % 3 != 0 and i % 5 == 0]]  # five_ugly_number
+        length, index, count, merge_index, re = len(ugly_number), [0] * len(ugly_number), 0, 0, 0
+        print ugly_number
+        re = []
+        while count < n:
+            merge = [ugly_number[i][index[i]] if index[i] < len(ugly_number[i]) else MAX_INT for i in range(length)]
+            merge_index = merge.index(min(merge))
+            re.append(ugly_number[merge_index][index[merge_index]])
+
+            index[merge_index] += 1
+            print merge, '-------------------', merge_index, '==========', index,'++++++',re
+
+            count += 1
+        return ugly_number[merge_index][index[merge_index] - 1]
+
+    def nthUglyNumber_264_V2(self, n):  # 遍历Time Limit Exceeded
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 1:
+            return 1
+        count, num = 1, 2
+        while True:
+            if self.isUgly_263(num):
+                count += 1
+                if count == n:
+                    return num
+            num += 1
+
+    def fractionAddition_592(self, expression):
+        """
+        :type expression: str
+        :rtype: str
+        """
+        def gcd(a, b):
+            if a < b:
+                a, b = b, a
+            while b:
+                a, b = b, a % b
+            return a
+
+        def lcm(a, b):
+            return a * b / gcd(a, b)
+
+        part = ''
+        fractions = []
+        for c in expression:
+            if c in '+-':
+                if part:
+                    fractions.append(part)
+                part = ''
+            part += c
+        if part:
+            fractions.append(part)
+
+        hi = [int(e.split('/')[0]) for e in fractions]
+        lo = [int(e.split('/')[1]) for e in fractions]
+
+        LO = reduce(lcm, lo)
+        HI = sum(h * LO / l for h, l in zip(hi, lo))
+        GCD = abs(gcd(LO, HI))
+
+        return '%s/%s' % (HI / GCD, LO / GCD)
+
+
+
+
 
 
 if __name__ == '__main__':
     s = Solution()
     #print(s.getRowV2(3))
     #print(s.removeDuplicatesV2([1, 1, 2, 2, 3]))
-    print(s.isUgly_263(-2147483648))
+    print(s.fractionAddition_592("-1/2-1/2"))
 
 
 
